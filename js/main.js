902a5572e9d7440dca4f060b816874e2947bd0ed
VKormylo <kvdev@users.noreply.github.com>
2020-05-27 14:05:46 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -9,32 +9,6 @@ $(function(){
       
     /* Fixed Header */
 
-    // let header = $('#header');
-    // let intro = $('#intro');
-    // let introH = intro.innerHeight();
-    // let scrollPos = $(window).scrollTop();
-    // let nav = $('#nav');
-    // let navToggle = $('#navToggle');
-
-    // checkScroll(scrollPos,introH);
-
-    // $(window).on('scroll resize', function() {
-    //    introH = intro.innerHeight();
-    //    scrollPos = $(this).scrollTop();
-       
-    //    checkScroll(scrollPos,introH);
-    // });
-
-    // function checkScroll(scrollPos,introH) {
-    //    if( scrollPos > introH ) {
-    //        header.addClass('fixed');
-    //    }  else {
-    //        header.removeClass('fixed');
-    //    }
-    // }
-
-
-
     $('[data-scroll]').on('click', function (event) {
       event.preventDefault();
 
@@ -65,11 +39,11 @@ $(function(){
        var height = $(window).scrollTop();  //getting the scrolling height of window
        if(height  > 900) {
          $(".header").css({"position": "fixed"});
-         $(".header-content").css({"opacity": "0"});
+         $(".header-content").addClass("hidden");
          $(".header-top").css({"background-color": "rgba(0, 0, 0, 0.200)"});
        } else{
          $(".header").css({"position": "absolute"});
-         $(".header-content").css({"opacity": "1"});
+         $(".header-content").removeClass("hidden");
          $(".header-top").css({"background-color": "rgba(255, 255, 255, 0)"});
        }
      });
